b22a6293a552253f3a78c3474da8fe6ec4c75db3
Denis Sergeev <dsergeev@ourgapps.com>
2025-01-22 13:24:08 +0400

    fixes by review

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"key-value-database/internal/database"
 	"key-value-database/internal/database/compute"
@@ -25,8 +23,6 @@ func main() {
 	}
 	logger.Debug("debug mode on")
 
-	waitExitSignal()
-
 	db := database.NewDatabase(&compute.CommandParser{}, storage.NewInMemoryEngine())
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -42,13 +38,4 @@ func main() {
 		}
 		fmt.Println(result)
 	}
-}
-
-func waitExitSignal() {
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-signals
-		os.Exit(0)
-	}()
 }
